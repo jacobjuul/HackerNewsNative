f29af1cd5aa258912425cede1e5e886872474f48
Jacob Juul Christiansen <jacob.juul@gmail.com>
2021-06-15 13:40:04 +0200

    navigate to the post screen

--- screens/PostScreen.tsx
+++ screens/PostScreen.tsx
@@ -1,11 +1,31 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
+import { useSelector } from 'react-redux'
+import { View, Text, StyleSheet, StatusBar, SafeAreaView } from 'react-native'
+import { OpenURLButton } from '../components/OpenURLButton'
 
 export const PostScreen = ({ navigation, route }) => {
   const { postId } = route.params
+  const post = useSelector((state) => state.posts.entities[postId])
+  console.log(post)
   return (
-    <View>
-      <Text>{postId}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <OpenURLButton
+        textStyle={styles.titleText}
+        url={post.url}
+        title={post.title}
+      />
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginVertical: 30,
+    marginHorizontal: 20,
+  },
+  titleText: {
+    fontSize: 20,
+  },
+})
